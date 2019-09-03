4e87404973815caeb8a7f2054f43747134afdd06
lshay <lshay@iastate.edu>
2019-09-03 11:23:32 -0500

    Added spring practice.

---
+++ Experiments/LukeSpring/src/main/java/com/javabrains/springboot/course/CourseController.java
@@ -0,0 +1,41 @@
+package com.javabrains.springboot.course;
+
+import com.javabrains.springboot.topic.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/topics/{id}/courses")
+    public List<Course> getAllCourses(@PathVariable String id) {
+        return courseService.getAllCourses(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/topics/{topicId}/courses/{courseId}")
+    public Course getCourse(@PathVariable String topicId, @PathVariable String courseId) {
+        return courseService.getCourse(courseId);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.addCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{courseId}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String topicId, @PathVariable String courseId) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.updateCourse(courseId, course);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "topics/{topicId}/courses/{courseId}")
+    public void deleteCourse(@PathVariable String courseId) {
+        courseService.deleteCourse(courseId);
+    }
+}
