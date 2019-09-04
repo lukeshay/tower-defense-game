e1cf065f5af59b2b6f9b080fbd50d2b66ab0379e
Tyler Krueger <tylerk@iastate.edu>
2019-09-03 19:30:16 -0500

    pc version

--- Experiments/TylerKruegerExp/src/HelloWorld.java
+++ Experiments/TylerKruegerExp/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Tyler Jeffery Krueger");
+        System.out.println("Git working pls");
     }
 }
