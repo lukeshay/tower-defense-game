49855fb6f6f7f33e7e862731d8bbe3aafd735dec
lshay <lshay@iastate.edu>
2019-09-05 20:56:33 -0500

    Added more spring boot practice projects.

---
+++ Experiments/RobertShay/movie-catalog-service/src/main/java/com/robertshay/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,48 @@
+package com.robertshay.moviecatalogservice.resources;
+
+import com.robertshay.moviecatalogservice.models.CatalogItem;
+import com.robertshay.moviecatalogservice.models.Movie;
+import com.robertshay.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /*@Autowired
+    private WebClient.Builder webClientBuilder;*/
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable String userId) {
+        // get all rated movie IDs (will be from rating api)
+        UserRating userRatings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
+
+        // put them all together
+        return userRatings.getRatings().stream().map(rating -> {
+            //// for each move ID, call info service
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMoveId(), Movie.class);
+            /* Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMoveId())
+                    .retrieve() // Sends the call and retrieves the body
+                    .bodyToMono(Movie.class) // Turns the body into the given class
+                    .block(); // Blocks command until this is done. Essentially makes it synchronous */
+
+            return new CatalogItem(movie.getName(), "Test desc", rating.getRating());
+        }).collect(Collectors.toList());
+    }
+}
--- Experiments/RobertSpring/src/main/java/com/javabrains/springboot/topic/TopicRepository.java
+++
@@ -1,6 +0,0 @@
-package com.javabrains.springboot.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String> {
-}
