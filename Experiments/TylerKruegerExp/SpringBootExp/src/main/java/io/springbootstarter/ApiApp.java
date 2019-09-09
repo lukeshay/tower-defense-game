0c4eb2506e653942a6838bd0bc5a777a224e2dbc
tylerkrueger <tylerk@iastate.edu>
2019-09-08 22:33:09 -0500

    SpringInitializr test

--- Experiments/TylerKruegerExp/SpringBootExp/src/main/java/io/springbootstarter/ApiApp.java
+++ Experiments/TylerKruegerExp/SpringBootExp/src/main/java/io/springbootstarter/ApiApp.java
@@ -1,7 +1,10 @@
 package io.springbootstarter;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApiApp {
     public static void main(String[] args){
-        
+
     }
 }
