1585a976cd1921e522c2746f87d032b98ba11731
tylerkrueger <tylerk@iastate.edu>
2019-09-09 22:06:24 -0500

    added ability to make a POST request

--- Experiments/TylerKruegerExp/InitializrExp/src/main/java/io/springbootquickstart/api/topic/TopicService.java
+++ Experiments/TylerKruegerExp/InitializrExp/src/main/java/io/springbootquickstart/api/topic/TopicService.java
@@ -2,16 +2,17 @@ package io.springbootquickstart.api.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
-                new Topic("spring", "Spring Framework", "Sring Framework Description"),
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Description"),
                 new Topic("java", "Core Java", "Core Java Description"),
-                new Topic("javascript", "JavaScript", "JavaScript Description"));
+                new Topic("javascript", "JavaScript", "JavaScript Description")));
 
     public List<Topic> getAllTopics(){
         return topics;
@@ -19,5 +20,9 @@ public class TopicService {
 
     public Topic getTopic(String id){
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic){
+        topics.add(topic);
     }
 }
