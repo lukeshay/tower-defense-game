2ccfe45b40a89a4918d7e3cb975c597852d53fa9
tylerkrueger <tylerk@iastate.edu>
2019-09-09 19:55:22 -0500

    more REST controllers

--- Experiments/TylerKruegerExp/InitializrExp/src/main/java/io/springbootquickstart/api/CourseApiApplication.java
+++ Experiments/TylerKruegerExp/InitializrExp/src/main/java/io/springbootquickstart/api/CourseApiApplication.java
@@ -3,6 +3,8 @@ package io.springbootquickstart.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class CourseApiApplication {
 
