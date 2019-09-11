570d3c7278d47e90ac9e1bc94a8180392138087e
tylerkrueger <tylerk@iastate.edu>
2019-09-11 15:55:47 -0500

    set up UsersService constructor

--- BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/users/services/UsersService.java
+++ BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/users/services/UsersService.java
@@ -1,7 +1,15 @@
 package com.pvptowerdefense.database.users.services;
 
+import com.pvptowerdefense.database.users.dao.UsersDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
+    private UsersDao usersDao;
+
+    @Autowired
+    public UsersService(UsersDao usersDao){
+        this.usersDao = usersDao;
+    }
 }
