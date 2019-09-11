b3f49e4e444643ab12341a2b8d56615c0651b810
lshay <lshay@iastate.edu>
2019-09-11 15:36:49 -0500

    Added fields to user and card.

--- BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/cards/models/Card.java
+++ BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/cards/models/Card.java
@@ -6,5 +6,39 @@ import javax.persistence.Id;
 @Entity
 public class Card {
     @Id
-    String name;
+    private String name;
+    private int ad;
+    private int hp;
+
+    private Card() {}
+
+    public Card(String name, int ad, int hp) {
+        this.name = name;
+        this.ad = ad;
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAd() {
+        return ad;
+    }
+
+    public void setAd(int ad) {
+        this.ad = ad;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 }
--- BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/users/models/User.java
+++ BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/users/models/User.java
@@ -1,11 +1,125 @@
 package com.pvptowerdefense.database.users.models;
 
+import com.pvptowerdefense.database.cards.models.Card;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.List;
 
 @Entity
 public class User {
     @Id
-    String userName;
+    private String phoneId;
 
+    @Column(unique=true)
+    private String userName;
+
+    @Column(unique=true)
+    private String email;
+    
+    private String firstName;
+    private String lastName;
+
+    private int xp;
+    private int trophies;
+
+    @ElementCollection
+    private List<Card> ownedCards;
+
+    @ElementCollection
+    private List<Card> currentDeck;
+
+    private User() {}
+
+    public User(String userName, String phoneId, String firstName, String lastName, String email, int xp, int trophies) {
+        this.userName = userName;
+        this.phoneId = phoneId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.xp = xp;
+        this.trophies = trophies;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhoneId() {
+        return phoneId;
+    }
+
+    public void setPhoneId(String phoneId) {
+        this.phoneId = phoneId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
+    public int getTrophies() {
+        return trophies;
+    }
+
+    public void setTrophies(int trophies) {
+        this.trophies = trophies;
+    }
+
+    public List<Card> getOwnedCards() {
+        return ownedCards;
+    }
+
+    public void setOwnedCards(List<Card> ownedCards) {
+        this.ownedCards = ownedCards;
+    }
+
+    public void addOwnedCard(Card card) {
+        ownedCards.add(card);
+    }
+
+    public List<Card> getCurrentDeck() {
+        return currentDeck;
+    }
+
+    public void setCurrentDeck(List<Card> currentDeck) {
+        this.currentDeck = currentDeck;
+    }
+
+    public void addToCurrentDeck(Card card) {
+        currentDeck.add(card);
+    }
 }
