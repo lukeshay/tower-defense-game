1b0ea6725bf78b29a02cf02e5b6ff874d81b215c
lshay <lshay@iastate.edu>
2019-09-11 12:19:47 -0500

    Fixed bug causing project to not run. Entities must have fields.

--- BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/cards/models/Card.java
+++ BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/cards/models/Card.java
@@ -1,8 +1,10 @@
 package com.pvptowerdefense.database.cards.models;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Card {
-
+    @Id
+    String name;
 }
--- BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/users/models/User.java
+++ BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/users/models/User.java
@@ -1,8 +1,11 @@
 package com.pvptowerdefense.database.users.models;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+    @Id
+    String userName;
 
 }
