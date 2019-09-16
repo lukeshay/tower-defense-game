c3b2f7a2716cc13f509bad5486a3d2a03a5628ef
tylerkrueger <tylerk@iastate.edu>
2019-09-16 13:07:15 -0500

    setting up UsersService and UserController classes

--- BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/users/dao/UsersDao.java
+++ BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/database/users/dao/UsersDao.java
@@ -4,4 +4,5 @@ import com.pvptowerdefense.database.users.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsersDao extends CrudRepository<User, String> {
+    public User getUserByPhoneId(String phoneId);
 }
