6cfd7ab08b3aafec0902b119349c3faa950c816f
Tyler Lund <tblund@iastate.edu>
2019-09-16 13:25:01 -0500

    added views and layouts for recyclerview to add functionality to the inventory

--- FrontEnd/app/src/main/java/com/example/myapplication/registerUserActivity.java
+++ FrontEnd/app/src/main/java/com/example/myapplication/registerUserActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +17,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.UUID;
 
 import static android.provider.Settings.Secure.ANDROID_ID;
 
@@ -27,34 +31,37 @@ public class registerUserActivity extends Activity {
         Button submit = (Button) findViewById(R.id.submitButton);
         final EditText screenName = (EditText) findViewById(R.id.usernameField);
 
+       final Intent intent = new Intent(this, inventoryActivity.class);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 URL url;
                 HttpURLConnection client = null;
                 try {
-                    url = new URL("hey this needs to be the server resource");
-                    client = (HttpURLConnection) url.openConnection();
-                    client.setRequestMethod("POST");
-                    client.setRequestProperty("Content-Type", "application/json; utf-8");
-                    client.setDoOutput(true);
-                    OutputStreamWriter osw = new OutputStreamWriter(client.getOutputStream());
-                    osw.write("{"+ screenName.getText() + ":" + generateUUID() + "}");
+                //    url = new URL("hey this needs to be the server resource");
+                //    client = (HttpURLConnection) url.openConnection();
+                //    client.setRequestMethod("POST");
+                //    client.setRequestProperty("Content-Type", "application/json; utf-8");
+                //    client.setDoOutput(true);
+                //    OutputStreamWriter osw = new OutputStreamWriter(client.getOutputStream());
+                //    osw.write("{"+ screenName.getText() + ":" + generateUUID() + "}");
+                    Log.e("user", "{" + screenName.getText() + ":" + generateUUID() + "}");
                 }
                 catch(Exception e){
                     e.printStackTrace();
                 }
                 try{
-                    BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream(), "utf-8"));
+                   // BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream(), "utf-8"));
                 }
                 catch(Exception e){
                     e.printStackTrace();
                 }
+                startActivity(intent);
             }
         });
     }
 
     private String generateUUID(){
-        return ANDROID_ID;
+       return Settings.Secure.getString(getApplicationContext().getContentResolver(), ANDROID_ID);
     }
 }
