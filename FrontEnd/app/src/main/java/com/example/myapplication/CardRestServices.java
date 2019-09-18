14b0587e098e68f114edd69cca2e579a2849b659
Zach <ztk@iastate.edu>
2019-09-18 15:48:45 -0500

    Set up a GET request to get cards from the server.

--- FrontEnd/app/src/main/java/com/example/myapplication/CardRestServices.java
+++ FrontEnd/app/src/main/java/com/example/myapplication/CardRestServices.java
@@ -1,13 +1,20 @@
 package com.example.myapplication;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A class used for posting cards to the back-end and for grabbing them.
  */
 public class CardRestServices {
+    private static String baseUrl = "http://coms-309-ss-5.misc.iastate.edu:8080/cards";
 
     public static void sendCardToDB(Card card){
         try {
@@ -26,6 +33,35 @@ public class CardRestServices {
         } catch(Exception e){
             //Handle exception
         }
+    }
+
+    public static List<Card> getAllCards(){
+        try {
+            URL url = new URL(baseUrl);
+            HttpURLConnection client = (HttpURLConnection) url.openConnection();
+            URLConnection con = url.openConnection();
+            client.setRequestMethod("GET");
+            client.setRequestProperty("Content-Type", "application/json; utf-8");
+            int responseCode = client.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String input;
+            StringBuffer response = new StringBuffer();
+            while((input = in.readLine()) != null){
+                response.append(input);
+            }
+            in.close();
+            //Debug
+            System.out.println(response.toString());
+        } catch(Exception e){
+            //Handle exception
+            System.out.println("Encountered error");
+        }
+        return new ArrayList<Card>();
+    }
+
+    public static void main(String[] args){
+        //Test connections
+        getAllCards();
     }
 
 
