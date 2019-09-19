ead8f63ce60cb1787fdd1ed4824ea3200e723a81
lshay <lshay@iastate.edu>
2019-09-18 19:33:43 -0500

    Added config.

--- BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/server/socket/SocketConfig.java
+++ BackEnd/DatabaseAccessProject/src/main/java/com/pvptowerdefense/server/socket/SocketConfig.java
@@ -1,4 +1,11 @@
 package com.pvptowerdefense.server.socket;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
 public class SocketConfig {
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter(){
+		return new ServerEndpointExporter();
+	}
 }
