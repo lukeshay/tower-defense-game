517094caab7bd32654a01d235b30e8825a72cea7
Tyler Lund <tblund@iastate.edu>
2019-09-20 16:35:54 -0500

    added volley implementation for get requests

---
+++ FrontEnd/app/src/main/java/com/example/myapplication/VolleyServices/VolleyUtilities.java
@@ -0,0 +1,41 @@
+package com.example.myapplication.VolleyServices;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class VolleyUtilities {
+
+
+    public static void getRequest(Context context, String url, final VolleyResponseListener listner){
+        final RequestQueue requestQueue = Volley.newRequestQueue(context);
+
+        // Initialize a new StringRequest
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Do something with response string
+                        listner.onResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Do something when get error
+                        listner.onError(error.toString());
+                    }
+                }
+        );
+
+        // Add StringRequest to the RequestQueue
+        VolleySingleton.getInstance(context).addToRequestQueue(stringRequest);
+    }
+}
