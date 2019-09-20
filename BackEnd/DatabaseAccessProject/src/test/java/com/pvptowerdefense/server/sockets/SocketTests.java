0fb3050caf6e400ed1823a7ae345b762e5453964
lshay <lshay@iastate.edu>
2019-09-19 21:09:21 -0500

    Working on writing tests for sockets.

--- BackEnd/DatabaseAccessProject/src/test/java/com/pvptowerdefense/server/sockets/SocketTests.java
+++ BackEnd/DatabaseAccessProject/src/test/java/com/pvptowerdefense/server/sockets/SocketTests.java
@@ -1,4 +1,49 @@
 package com.pvptowerdefense.server.sockets;
 
+import org.java_websocket.client.WebSocketClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SocketTests {
+	private static final String URL = "http://localhost:8080/socket/";
+
+	private WebSocketStompClient socketOne;
+	private WebSocketStompClient socketTwo;
+
+	private WebSocketStompClient socket() throws URISyntaxException {
+		StandardWebSocketClient simpleWebSocketClient = new StandardWebSocketClient();
+		List<Transport> transports = new ArrayList<>(1);
+		transports.add(new WebSocketTransport(simpleWebSocketClient));
+		SockJsClient sockJsClient = new SockJsClient(transports);
+		WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+		return stompClient;
+	}
+
+	@BeforeEach
+	public void setUpSockets() throws URISyntaxException {
+		socketOne = socket();
+		socketTwo = socket();
+	}
+	
+	@Test
+	public void testCreateGameEndpoint() throws URISyntaxException {
+	}
 }
