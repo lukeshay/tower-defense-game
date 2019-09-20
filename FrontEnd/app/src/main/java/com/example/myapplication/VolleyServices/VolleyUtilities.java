c052f44da20661099859afef40ddc275fb9c31e4
Tyler Lund <tblund@iastate.edu>
2019-09-20 16:59:30 -0500

    added volley post request

--- FrontEnd/app/src/main/java/com/example/myapplication/VolleyServices/VolleyUtilities.java
+++ FrontEnd/app/src/main/java/com/example/myapplication/VolleyServices/VolleyUtilities.java
@@ -9,6 +9,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.Map;
+
 public class VolleyUtilities {
 
 
@@ -34,6 +36,39 @@ public class VolleyUtilities {
                     }
                 }
         );
+
+        // Add StringRequest to the RequestQueue
+        VolleySingleton.getInstance(context).addToRequestQueue(stringRequest);
+    }
+
+    public static void postRequest(Context context, String url, final VolleyResponseListener listner, Map<String, String> params){
+        final RequestQueue requestQueue = Volley.newRequestQueue(context);
+
+        // Initialize a new StringRequest
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Do something with response string
+                        listner.onResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Do something when get error
+                        listner.onError(error.toString());
+                    }
+                }
+
+        ){
+            @Override
+            protected Map<String, String> getParams(){
+                return params;
+            }
+        };
 
         // Add StringRequest to the RequestQueue
         VolleySingleton.getInstance(context).addToRequestQueue(stringRequest);
