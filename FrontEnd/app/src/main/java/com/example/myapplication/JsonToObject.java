cd02a8a527360ff2ad086895bdba968523612adf
Tyler Lund <tblund@iastate.edu>
2019-09-20 12:40:32 -0500

    added in gson class for json to object and added implementation to gradle build

---
+++ FrontEnd/app/src/main/java/com/example/myapplication/JsonToObject.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class JsonToObject {
+
+    public static Card convert(String json){
+        Gson test = new Gson();
+        return test.fromJson(json, Card.class);
+    }
+
+    public static Collection<Card> convertArray(String json){
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<Collection<Card>>(){}.getType();
+        Collection<Card> enums = gson.fromJson(json, collectionType);
+        return enums;
+    }
+
+
+}
