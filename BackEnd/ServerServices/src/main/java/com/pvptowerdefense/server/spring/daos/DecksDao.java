77650392d118a13aa0a83bbf2e69100d3abd7a8b
tylerkrueger <tylerk@iastate.edu>
2019-10-13 19:44:01 -0500

    getting deck service set up

--- BackEnd/ServerServices/src/main/java/com/pvptowerdefense/server/spring/daos/DecksDao.java
+++ BackEnd/ServerServices/src/main/java/com/pvptowerdefense/server/spring/daos/DecksDao.java
@@ -1,4 +1,9 @@
 package com.pvptowerdefense.server.spring.daos;
 
-public interface DecksDao {
+import com.pvptowerdefense.server.spring.models.Deck;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DecksDao extends CrudRepository<Deck, String> {
 }
--- BackEnd/ServerServices/src/main/java/com/pvptowerdefense/server/spring/services/DecksService.java
+++ BackEnd/ServerServices/src/main/java/com/pvptowerdefense/server/spring/services/DecksService.java
@@ -1,4 +1,25 @@
 package com.pvptowerdefense.server.spring.services;
 
+import com.pvptowerdefense.server.spring.daos.DecksDao;
+import com.pvptowerdefense.server.spring.models.Deck;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class DecksService {
+
+    private DecksDao decksDao;
+
+    @Autowired
+    public DecksService(DecksDao decksDao){ this.decksDao = decksDao; }
+
+    public List<Deck> getAllDecks(){
+        List<Deck> decks = new ArrayList<Deck>();
+        decksDao.findAll().forEach(decks::add);
+
+        return decks;
+    }
 }
