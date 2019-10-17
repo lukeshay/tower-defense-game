6d704933405fb4a70999e5790821b6c71c8bc6e1
tylerkrueger <tylerk@iastate.edu>
2019-10-17 10:28:33 -0500

    created basic map of played cards

--- BackEnd/ServerServices/src/main/java/com/pvptowerdefense/server/socket/models/Map.java
+++ BackEnd/ServerServices/src/main/java/com/pvptowerdefense/server/socket/models/Map.java
@@ -1,4 +1,18 @@
 package com.pvptowerdefense.server.socket.models;
 
 public class Map {
+
+    private PlayedCard[][] cardsInPlay;
+
+    public Map(){
+        cardsInPlay = new PlayedCard[1500][500];
+    }
+
+    public PlayedCard[][] getCardsInPlay() {
+        return cardsInPlay;
+    }
+
+    public void setCardsInPlay(PlayedCard[][] cardsInPlay) {
+        this.cardsInPlay = cardsInPlay;
+    }
 }
