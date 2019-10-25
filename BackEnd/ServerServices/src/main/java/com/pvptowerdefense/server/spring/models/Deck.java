861a0a9be3f849ada3f992c0d51db1d413c8d06a
tylerkrueger <tylerk@iastate.edu>
2019-10-25 15:25:43 -0500

    javadoc Deck.java

--- BackEnd/ServerServices/src/main/java/com/pvptowerdefense/server/spring/models/Deck.java
+++ BackEnd/ServerServices/src/main/java/com/pvptowerdefense/server/spring/models/Deck.java
@@ -9,6 +9,9 @@ import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Deck type class
+ */
 @Entity
 public class Deck {
 
@@ -22,34 +25,63 @@ public class Deck {
     @Column(unique = true, name = "DECK_NAME", nullable = false)
     private String deckName;
 
+    /**
+     * Creates an empty deck
+     */
     public Deck(){
         this.deck = new ArrayList<>();
     }
 
+    /**
+     * Creates a deck from given parameters
+     * @param deck - list of cards, possibly empty
+     * @param deckName - the name for the deck
+     * @param deckId - the unique deckId
+     */
     public Deck(List<Card> deck, String deckName, int deckId) {
         this.deck = deck;
         this.deckName = deckName;
         this.deckId = deckId;
     }
 
+    /**
+     * Gets the list of cards in the user's current deck
+     * @return list of cards
+     */
     public List<Card> getDeck() {
         return deck;
     }
 
+    /**
+     * Sets the list of cards to the desired list for the deck
+     * @param deck - list of cards to be changed to
+     */
     public void setDeck(List<Card> deck) {
         this.deck = deck;
     }
 
+    /**
+     * Gets the current deck's name
+     * @return the deck name
+     */
     public String getDeckName() {
         return deckName;
     }
 
+    /**
+     * Sets the deck name to the desired name
+     * @param deckName - name to change the deck name to
+     */
     public void setDeckName(String deckName) {
         this.deckName = deckName;
     }
 
+    /**
+     * Adds a new card to the deck
+     * @param cardName - card to be added
+     */
     public void addCard(Card cardName){
-
+        deck.add(cardName);
     }
 }
 
