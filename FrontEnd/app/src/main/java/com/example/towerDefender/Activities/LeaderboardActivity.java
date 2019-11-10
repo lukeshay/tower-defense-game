8978b065ae47423cf9b528cb463d05e70f4a07bc
Zach Kauffman <ztk@iastate.edu>
2019-11-10 00:17:15 -0600

    Worked on leaderboard activity. Displays users in order but visual touchups are needed

--- FrontEnd/app/src/main/java/com/example/towerDefender/Activities/LeaderboardActivity.java
+++ FrontEnd/app/src/main/java/com/example/towerDefender/Activities/LeaderboardActivity.java
@@ -2,10 +2,14 @@ package com.example.towerDefender.Activities;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.towerDefender.R;
 import com.example.towerDefender.Util.LeaderboardUtility;
 import com.example.towerDefender.VolleyServices.CardRestServices;
 import com.example.towerDefender.VolleyServices.JsonUtils;
@@ -21,7 +25,7 @@ public class LeaderboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.example.towerDefender.R.layout.activity_leaderboard);
-        VolleyUtilities.getRequest(this.getApplicationContext(), CardRestServices.BASE_URL, new VolleyResponseListener() {
+        VolleyUtilities.getRequest(this.getApplicationContext(), UserRestServices.BASE_URL, new VolleyResponseListener() {
             @Override
             public void onError(String message) {
                 Log.e("ERROR", "Encountered an error while grabbing cards from database. " + message);
@@ -39,11 +43,24 @@ public class LeaderboardActivity extends AppCompatActivity {
         User[] userArray = new User[users.size()];
         users.toArray(userArray);
         sortUsers(userArray);
+        displayUsers(userArray);
         Log.i("leaderboard_info", userArray.toString());
     }
 
     private void sortUsers(User[] users){
         LeaderboardUtility.mergeSort(users);
+    }
+
+    private void displayUsers(User[] users){
+        LinearLayout layout = this.findViewById(R.id.leaderboardLayout);
+        for (User user : users) {
+            TextView textView = new TextView(this);
+            textView.setText(user.phoneId + ": " + user.trophyCount + " trophies.");
+            //textView.setTextColor(getResources().getColor(R.color.colorAccent));
+            textView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            layout.addView(textView);
+        }
     }
 
 
